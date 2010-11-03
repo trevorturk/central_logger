af465e0d44b298e1f8bcc44bb627288678e8ac7d
Trevor Turk <trevorturk@gmail.com>
2010-11-03 17:28:54 +0000

    filter request params in rails 3

--- lib/central_logger/filter.rb
+++ lib/central_logger/filter.rb
@@ -7,14 +7,12 @@ module CentralLogger
     def enable_central_logger
       return yield unless Rails.logger.respond_to?(:mongoize)
 
-      # make sure the controller knows how to filter its parameters
-      f_params = respond_to?(:filter_parameters) ? filter_parameters(params) : params
       Rails.logger.mongoize({
         :action         => action_name,
         :controller     => controller_name,
         :path           => request.path,
         :url            => request.url,
-        :params         => f_params,
+        :params         => request.filtered_parameters,
         :ip             => request.remote_ip
       }) { yield }
     end
