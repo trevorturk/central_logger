f6a2736a5c6c5cbdb90f87bc0bfba7635d34a19f
Trevor Turk <trevorturk@gmail.com>
2010-12-01 12:36:17 +0000

    initialize central_logger after rails logger to keep colored logs

--- lib/central_logger/railtie.rb
+++ lib/central_logger/railtie.rb
@@ -6,7 +6,7 @@ if Rails::VERSION::MAJOR == 3
 
     # load earlier than bootstrap.rb initializer loads the default logger.  bootstrap
     # initializer will then skip its own initialization once Rails.logger is defined
-    initializer :initialize_central_logger, :before => :initialize_logger do
+    initializer :initialize_central_logger, :after => :initialize_logger do
       app_config = Rails.application.config
       Rails.logger = config.logger = create_logger(app_config,
                                                    app_config.paths.log.to_a.first)
